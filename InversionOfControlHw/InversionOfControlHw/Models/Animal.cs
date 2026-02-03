9d0a7be02804c9462cb1bafcc5a1cea7bcdfbd78
ksovreli <sandroqsovreli@gmail.com>
2026-02-03 21:07:22 +0400

    changes were made

---
+++ InversionOfControlHw/InversionOfControlHw/Models/Animal.cs
@@ -0,0 +1,9 @@
+﻿namespace InversionOfControlHw.Models
+{
+    internal abstract class Animal(string name, uint weight)
+    {
+        public string Name { get; set; } = name;
+        public uint Weight { get; set; } = weight;
+        public abstract void Sound();
+    }
+}
---
+++ InversionOfControlHw/InversionOfControlHw/Models/Bear.cs
@@ -0,0 +1,15 @@
+﻿namespace InversionOfControlHw.Models
+{
+    internal class Bear: Animal
+    {
+        public Bear(string name, uint weight): base(name, weight)
+        {
+            
+        }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Roarrrr!");
+        }
+    }
+}
