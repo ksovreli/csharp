2f6e7b5f29ef64dd32df8a3186cd1b1e8a83930c
xyzape <sandroqsovreli@gmail.com>
2026-03-02 15:48:05 +0400

    Delete InversionOfControlHw directory

--- InversionOfControlHw/InversionOfControlHw/Models/Animal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InversionOfControlHw.Models
-{
-    internal abstract class Animal(string name, uint weight)
-    {
-        public string Name { get; set; } = name;
-        public uint Weight { get; set; } = weight;
-        public abstract void Sound();
-    }
-}
